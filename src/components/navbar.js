56e45908fd3033f54b88aee40090726a737e9de1
kris <krisekteles@gmail.com>
2025-07-26 20:51:53 +0300

    navbar lists

---
+++ src/components/navbar.js
@@ -0,0 +1,26 @@
+import "./navbar.css";
+function Navbar() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <a href="">
+              <h2>E-HOSPITAL</h2>
+            </a>
+          </li>
+          <li>
+            <a href="#">Visit us</a>
+          </li>
+          <li>
+            <a href="#">Ambulance</a>
+          </li>
+          <li>
+            <a href="#">About us</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+export default Navbar;
