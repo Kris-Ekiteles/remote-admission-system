d7ddae56af574c595e18842484326570831b54a8
kris <krisekteles@gmail.com>
2025-07-27 14:46:25 +0300

    changes to button adding classname

--- src/components/landing.js
+++ src/components/landing.js
@@ -2,7 +2,7 @@ import "./landing.css";
 function FormCheck() {
   return (
     <div>
-      <h2>main app</h2>
+      {/* <h2>main app</h2> */}
       <div className="Hero">
         <div className="form-container">
           <h2>welcome to e-hospital</h2>
@@ -11,7 +11,7 @@ function FormCheck() {
             <input type="text" placeholder="username" required></input>
             <br></br>
             <label for="email">email</label>
-            <input type="email" placeholder="Enter email" required></input>
+            <input type="email" placeholder="Enter email"></input>
             <br></br>
             <label for="tell">phone number</label>
             <input type="tel" placeholder="e.g +2547123456789"></input>
@@ -19,8 +19,10 @@ function FormCheck() {
             <label for="text">reason for visit</label>
             <textarea id="text" placeholder="write your reason here"></textarea>
             <br></br>
-            <input type="date"></input>
-            <label for="single-select">Depertment you wish to visit:</label>
+            <input type="date" required></input>
+            <label for="single-select" required>
+              Depertment you wish to visit:
+            </label>
             <select id="single-select" name="single-select">
               <option value="EA">Emergency and accident</option>
               <option value="MD">Medical department</option>
@@ -29,7 +31,7 @@ function FormCheck() {
               <option value="MRI">MRI</option>
               <option value="specialist">other specialists</option>
             </select>
-            <button>submit</button>
+            <button className="btn">submit</button>
           </form>
         </div>
       </div>
