8af629e1fb7a3027fc400a2f4ba8a9082daddeb9
kris <krisekteles@gmail.com>
2025-07-27 21:25:48 +0300

    add another div for the buttons

--- src/components/about.js
+++ src/components/about.js
@@ -1,12 +1,14 @@
 import "./about.css";
 function About() {
   return (
-  <>
-  <div className="About_us">
-  <button className="btn1">click me</button>
-  <button className="btn2">more</button>
-  </div>
-  </>
+    <>
+      <div className="About_us">
+        <div className="About_buttons">
+          <button className="btn1">click me</button>
+          <button className="btn2">more</button>
+        </div>
+      </div>
+    </>
   );
 }
 export default About;
