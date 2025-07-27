c19207b258ec1b3f4a11a33dddc907884da61068
kris <krisekteles@gmail.com>
2025-07-27 14:42:48 +0300

    buttons added

---
+++ src/components/about.js
@@ -0,0 +1,12 @@
+import "./about.css";
+function About() {
+  return (
+  <>
+  <div className="About_us">
+  <button className="btn1">click me</button>
+  <button className="btn2">more</button>
+  </div>
+  </>
+  );
+}
+export default About;
