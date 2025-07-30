c7f19453f0bf5867481c049ed60411b26e826fe4
kris <krisekteles@gmail.com>
2025-07-30 12:37:02 +0300

    routing in progress for navbar component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import FormCheck from "./components/landing";
 import Navbar from "./components/navbar";
-import About from "./components/about";
-import Ambulance from "./components/ambulance";
+// import About from "./components/about";
+// import Ambulance from "./components/ambulance";
 
 function App() {
   return (
     <div className="App">
       < Navbar />
-      <FormCheck />
-      < About />
-      < Ambulance />
+      <div className="container">
+        <Routes>
+          <Route path="/home" element={ <landing />}/>
+        </Routes>
+      </div>
     </div>
   );
 }
